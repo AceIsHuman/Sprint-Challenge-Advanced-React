a92720a011be84781e893b1611c81bf8f0e66776
AceIsHuman <menoman1995@yahoo.com>
2019-09-14 12:06:27 -0500

    render PlayersList to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import PlayersList from './components/PlayersList';
+
 function App() {
   return (
     <div className="App">
       <section className="container">
-
+        <PlayersList />
       </section>
     </div>
   );
