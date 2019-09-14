217b7ba52d9106128a0f748958554d73e6545411
AceIsHuman <menoman1995@yahoo.com>
2019-09-14 13:29:21 -0500

    test if favorite players component is rendered

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,5 +3,10 @@ import * as rtl from '@testing-library/react';
 import App from './App';
 
 test('renders without crashing', () => {
-  rtl.render(<App />)
+  rtl.render(<App />);
+});
+
+test('favorite players component is rendered', () => {
+  const { getByTestId } = rtl.render(<App />);
+  getByTestId(/favorite-players/i);
 });
